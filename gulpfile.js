0ab020adc7c2a292b1896fe8b1d2b58fd4bb6403
Seshi Chemudugunta <seshi@Seshis-MacBook-Pro.local>
2016-02-19 01:50:30 +0900

    Added eslint and sourcemapping.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,24 @@
 var gulp = require("gulp");
 var babel = require("gulp-babel");
+var eslint = require('gulp-eslint');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
 
-gulp.task("default", function () {
+gulp.task("default", ['clean', 'lint'], function () {
     return gulp.src("index.js")
+        .pipe(sourcemaps.init())
         .pipe(babel())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest("lib"));
+});
+
+gulp.task('clean', function(cb) {
+    del(['lib']).then(function() { cb(); });
+});
+
+gulp.task('lint', function() {
+    return gulp.src('index.js')
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Peripheral } from 'raspi-peripheral';
 import addon from '../build/Release/raspiSonarAddon';
 
+// Sonar class provides a read method to interact with an ultrasonic sensor
 export class Sonar extends Peripheral {
     constructor(config) {
         super(config);
@@ -8,6 +9,7 @@ export class Sonar extends Peripheral {
         this.sensor = addon.sonarFactory(config); 
     }
     
+    // accepts function as an argument which is called back with a duration
     read(callback) {
         this.sensor.read(callback);
     }
